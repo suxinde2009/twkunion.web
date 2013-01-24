ab071b19ace17a2e0baaef72cf64f832469cd127
Jerry Shen <hansay99@gmail.com>
2013-01-24 14:59:41 +0800

    add fallback image

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -20,13 +20,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  #Provide a default URL as a default if there hasn't been a file uploaded:
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    asset_path("fallback/topic_placeholder.jpg")
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
